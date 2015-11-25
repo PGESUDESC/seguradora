4ba0e746621644b86c6934a7f4a8089338c7d8e4
Leandro.Battisti <leandro.battisti@senior.com.br>
2015-11-25 16:09:39 -0200

    Versão nova com MVC5

---
+++ Seguradora/Seguradora/Controllers/SeguradoesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Seguradora;
+
+namespace Seguradora.Controllers
+{
+    public class SeguradoesController : Controller
+    {
+        private seguradoraEntities db = new seguradoraEntities();
+
+        // GET: Seguradoes
+        public ActionResult Index()
+        {
+            var segurado = db.Segurado.Include(s => s.ObjetoSegurado1);
+            return View(segurado.ToList());
+        }
+
+        // GET: Seguradoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Segurado segurado = db.Segurado.Find(id);
+            if (segurado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(segurado);
+        }
+
+        // GET: Seguradoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.Codigo = new SelectList(db.ObjetoSegurado, "Codigo", "TipoAutomovel");
+            return View();
+        }
+
+        // POST: Seguradoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Codigo,Nome,Documento,DataNascimento,Sexo,EstadoCivil,FoneResidencial,FoneCelular,Email,Rua,Numero,Bairro,CEP,Cidade,Estado,BonusAtual,SeguradoraAnterior,NumeroCNH,PrimeiraHabilitacao")] Segurado segurado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Segurado.Add(segurado);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Codigo = new SelectList(db.ObjetoSegurado, "Codigo", "TipoAutomovel", segurado.Codigo);
+            return View(segurado);
+        }
+
+        // GET: Seguradoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Segurado segurado = db.Segurado.Find(id);
+            if (segurado == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Codigo = new SelectList(db.ObjetoSegurado, "Codigo", "TipoAutomovel", segurado.Codigo);
+            return View(segurado);
+        }
+
+        // POST: Seguradoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Codigo,Nome,Documento,DataNascimento,Sexo,EstadoCivil,FoneResidencial,FoneCelular,Email,Rua,Numero,Bairro,CEP,Cidade,Estado,BonusAtual,SeguradoraAnterior,NumeroCNH,PrimeiraHabilitacao")] Segurado segurado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(segurado).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Codigo = new SelectList(db.ObjetoSegurado, "Codigo", "TipoAutomovel", segurado.Codigo);
+            return View(segurado);
+        }
+
+        // GET: Seguradoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Segurado segurado = db.Segurado.Find(id);
+            if (segurado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(segurado);
+        }
+
+        // POST: Seguradoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Segurado segurado = db.Segurado.Find(id);
+            db.Segurado.Remove(segurado);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
