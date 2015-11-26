88c1fd9fad4ec76f33844354a35507419038f360
Principal <asengardeons@hotmail.com>
2015-11-26 21:03:13 -0200

    Adicionado suporte a datetimepicker para os campos data

--- Seguradora/Seguradora/App_Start/BundleConfig.cs
+++ Seguradora/Seguradora/App_Start/BundleConfig.cs
@@ -21,10 +21,13 @@ namespace Seguradora
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-datepicker.js",
+                      "~/Scripts/locales/bootstrap-datepicker.pt-BR.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-datepicker3.css",
                       "~/Content/site.css"));
         }
     }
