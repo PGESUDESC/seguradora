48510fd856fae924bfd4569b8e3d9267e8ac207f
Ricardo Voigt <voigtt@gmail.com>
2015-11-26 23:19:43 -0200

    url Helper/MyUrlHelper.cs

---
+++ Seguradora/Seguradora/Helper/MyUrlHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace System.Web.Mvc
+{
+
+    public static class MyUrlHelpers
+    {
+        public static string Image(this UrlHelper helper, string fileName)
+        {
+            return helper.Content(string.Format("~/Images/{0}", fileName));
+        }
+    }
+}
