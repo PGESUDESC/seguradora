6eef3713abefda6827b969e83abfafdd08345246
Ricardo Voigt <voigtt@gmail.com>
2015-12-01 22:54:09 -0200

    Adicionado combos de sexo e estado civil

---
+++ Seguradora/Seguradora/Comum/EstadoCilvilEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Seguradora.Comum
+{
+    public enum EstadoCilvilEnum
+    {
+        Casado = 1,
+        Solteiro = 2,
+        Divorciado = 3,
+        Viuvo = 4
+    }
+}
---
+++ Seguradora/Seguradora/Comum/SexoEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Seguradora.Comum
+{
+    public enum SexoEnum
+    {
+        Masculino = 1,
+        Feminino = 2
+    }
+}
